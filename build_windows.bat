74576fdfa1e33c47269b3509d6a5d3f661c9d86c
unknown <alt.el-foguf36l@yopmail.com>
2015-05-11 16:07:19 +0200

    echo format fix

--- build_windows.bat
+++ build_windows.bat
@@ -4,40 +4,40 @@ mkdir deps
 cd %src_path%\deps
 
 :Write d/l JScript
-echo var url = WScript.Arguments(0), >getfile.js
-echo  filename = WScript.Arguments(1), >>getfile.js
-echo   fso = WScript.CreateObject('Scripting.FileSystemObject'), >>getfile.js
-echo   request, stream; >>getfile.js
-echo if (fso.FileExists(filename)) { >>getfile.js
-echo   WScript.Echo(Already got  + filename); >>getfile.js
-echo } else { >>getfile.js
-echo   WScript.Echo('Downloading ' + url); >>getfile.js
-echo   request = WScript.CreateObject('MSXML2.ServerXMLHTTP'); >>getfile.js
-echo   request.open('GET', url, false); >>getfile.js
-echo   request.send(); >>getfile.js
-echo   if (request.status === 200) { >>getfile.js
-echo     WScript.Echo('Size: ' + request.getResponseHeader('Content-Length') + ' bytes'); >>getfile.js
-echo     stream = WScript.CreateObject('ADODB.Stream'); >>getfile.js
-echo     stream.Open(); >>getfile.js
-echo     stream.Type = 1; >>getfile.js
-echo     stream.Write(request.responseBody); >>getfile.js
-echo     stream.Position = 0; >>getfile.js
-echo     stream.SaveToFile(filename, 1); >>getfile.js
-echo     stream.Close(); >>getfile.js
-echo     WScript.Echo('Done'); >>getfile.js
-echo   } else { >>getfile.js
-echo     WScript.Echo('Failed'); >>getfile.js
-echo     WScript.Quit(1); >>getfile.js
-echo   } >>getfile.js
-echo } >>getfile.js
-echo WScript.Quit(0); >>getfile.js
+echo var url = WScript.Arguments(0),>getfile.js
+echo   filename = WScript.Arguments(1),>>getfile.js
+echo   fso = WScript.CreateObject('Scripting.FileSystemObject'),>>getfile.js
+echo   request, stream;>>getfile.js
+echo if (fso.FileExists(filename)) {>>getfile.js
+echo   WScript.Echo('Already got ' + filename);>>getfile.js
+echo } else {>>getfile.js
+echo   WScript.Echo('Downloading ' + url);>>getfile.js
+echo   request = WScript.CreateObject('MSXML2.ServerXMLHTTP');>>getfile.js
+echo   request.open('GET', url, false);>>getfile.js
+echo   request.send();>>getfile.js
+echo   if (request.status === 200) {>>getfile.js
+echo     WScript.Echo('Size: ' + request.getResponseHeader('Content-Length') + ' bytes');>>getfile.js
+echo     stream = WScript.CreateObject('ADODB.Stream');>>getfile.js
+echo     stream.Open();>>getfile.js
+echo     stream.Type = 1;>>getfile.js
+echo     stream.Write(request.responseBody);>>getfile.js
+echo     stream.Position = 0;>>getfile.js
+echo     stream.SaveToFile(filename, 1);>>getfile.js
+echo     stream.Close();>>getfile.js
+echo     WScript.Echo('Done');>>getfile.js
+echo   } else {>>getfile.js
+echo     WScript.Echo('Failed');>>getfile.js
+echo     WScript.Quit(1);>>getfile.js
+echo   }>>getfile.js
+echo }>>getfile.js
+echo WScript.Quit(0);>>getfile.js
 
 :Write unzip VBScript
-echo pathToZipFile=Wscript.Arguments(0) >unzip.vbs
-echo extractTo=Wscript.Arguments(1) >>unzip.vbs
-echo set sa = CreateObject('Shell.Application') >>unzip.vbs
-echo set filesInzip=sa.NameSpace(pathToZipFile).items >>unzip.vbs
-echo sa.NameSpace(extractTo).CopyHere(filesInzip) >>unzip.vbs
+echo pathToZipFile=Wscript.Arguments(0)>unzip.vbs
+echo extractTo=Wscript.Arguments(1)>>unzip.vbs
+echo set sa = CreateObject("Shell.Application")>>unzip.vbs
+echo set filesInzip=sa.NameSpace(pathToZipFile).items>>unzip.vbs
+echo sa.NameSpace(extractTo).CopyHere(filesInzip)>>unzip.vbs
 
 :Boost download
 cscript //e:jscript getfile.js http://freefr.dl.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.zip boost_1_58_0.zip
